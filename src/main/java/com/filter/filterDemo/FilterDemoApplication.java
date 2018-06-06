23d2308eea3284416c5f5c617ddcb26560e4d695
lyf451296108 <451296108@qq.com>
2018-06-06 10:28:08 +0800

    init project

---
+++ src/main/java/com/filter/filterDemo/FilterDemoApplication.java
@@ -0,0 +1,12 @@
+package com.filter.filterDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/filter/filterDemo/FilterDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.filter.filterDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FilterDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
